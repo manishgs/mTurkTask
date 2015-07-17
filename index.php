7651de61a076dab2aaf31acb9d0b9daf1359fdd9
Deepak Shah <deepak.shah@yipl.com.np>
2015-07-17 11:09:50 +0545

    intialized

---
+++ index.php
@@ -0,0 +1,8 @@
+<h4>Thanks for working on this task!</h4>
+<form id="mturk_form" method="post" action="https://workersandbox.mturk.com/mturk/externalSubmit">
+    <input type="hidden" name="workerId" value="" />
+    <input type="hidden" name="assignmentId" value="" />
+    <textarea name="feedback" rows=10 cols=60 placeholder="Enter any feedback you have about the task here"></textarea>
+    <br /><br />
+    <button class="btn btn-success" type="submit" value="Submit">Finish and Submit HIT</button>
+</form>
